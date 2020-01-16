0e054258a0bb33e9919c1ab20c839e16db470a2c
alexchew95 <37780051+alexchew95@users.noreply.github.com>
2020-01-16 21:45:58 +0800

    new gridview with todo

--- app/src/main/java/fyp/chewtsyrming/smartgrocery/adapter/GoodsListGridAdapter.java
+++ app/src/main/java/fyp/chewtsyrming/smartgrocery/adapter/GoodsListGridAdapter.java
@@ -16,8 +16,10 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
+
 import java.util.List;
 import java.util.Locale;
+
 import fyp.chewtsyrming.smartgrocery.R;
 import fyp.chewtsyrming.smartgrocery.object.GoodsGrid;
 import fyp.chewtsyrming.smartgrocery.object.UserModel;
@@ -25,8 +27,9 @@ import fyp.chewtsyrming.smartgrocery.object.UserModel;
 public class GoodsListGridAdapter extends BaseAdapter {
     private final Context mContext;
     private final List<GoodsGrid> goodsSpecific;
+    UserModel um;
     private List<GoodsGrid> filteredList;
-UserModel um;
+
     public GoodsListGridAdapter(Context mContext, List<GoodsGrid> goodsSpecific) {
         this.mContext = mContext;
         this.goodsSpecific = goodsSpecific;
@@ -37,21 +40,22 @@ UserModel um;
     public int getCount() {
         return goodsSpecific.size();
     }
+
     @Override
     public long getItemId(int position) {
         return 0;
     }
+
     @Override
     public Object getItem(int position) {
         return goodsSpecific.get(position);
     }
 
 
-
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-um = new UserModel();
-String userId= um.getUserIDFromDataBase();
+        um = new UserModel();
+        String userId = um.getUserIDFromDataBase();
         final GoodsGrid book = goodsSpecific.get(position);
 
         if (convertView == null) {
@@ -64,22 +68,22 @@ String userId= um.getUserIDFromDataBase();
             final TextView nameTextView = convertView.findViewById(R.id.textview_goods_name);
 
 
-            final ViewHolder viewHolder = new GoodsListGridAdapter.ViewHolder(nameTextView, imageView,imageview_favorite);
+            final ViewHolder viewHolder = new GoodsListGridAdapter.ViewHolder(nameTextView, imageView, imageview_favorite);
             convertView.setTag(viewHolder);
         }
 
         final ViewHolder viewHolder = (ViewHolder) convertView.getTag();
 //    viewHolder.imageViewCoverArt.setImageResource(book.getImageResource());
-        Picasso.get().load(book.getImageUrl()).fit().into( viewHolder.imageView);
+        Picasso.get().load(book.getImageUrl()).fit().into(viewHolder.imageView);
         //viewHolder.imageView.setImageResource(book.getImageResource());
         viewHolder.nameTextView.setText(book.getName());
-        DatabaseReference favReff= FirebaseDatabase.getInstance().getReference().
+        DatabaseReference favReff = FirebaseDatabase.getInstance().getReference().
                 child("user").child(userId).child("goods").child("fav");
         favReff.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(dataSnapshot.hasChild(book.getBarcode())){
-viewHolder.imageview_favorite.setImageResource(R.drawable.ic_enabled_star);
+                if (dataSnapshot.hasChild(book.getBarcode())) {
+                    viewHolder.imageview_favorite.setImageResource(R.drawable.ic_enabled_star);
                 }
             }
 
@@ -90,6 +94,21 @@ viewHolder.imageview_favorite.setImageResource(R.drawable.ic_enabled_star);
         });
 
         return convertView;
+    }
+
+    public void filter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        goodsSpecific.clear();
+        if (charText.length() == 0) {
+            goodsSpecific.addAll(filteredList);
+        } else {
+            for (GoodsGrid wp : filteredList) {
+                if (wp.getName().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    goodsSpecific.add(wp);
+                }
+            }
+        }
+        notifyDataSetChanged();
     }
 
     private class ViewHolder {
@@ -105,20 +124,6 @@ viewHolder.imageview_favorite.setImageResource(R.drawable.ic_enabled_star);
             this.imageview_favorite = imageview_favorite;
 
         }
-    }
-    public void filter(String charText) {
-        charText = charText.toLowerCase(Locale.getDefault());
-        goodsSpecific.clear();
-        if (charText.length() == 0) {
-            goodsSpecific.addAll(filteredList);
-        } else {
-            for (GoodsGrid wp : filteredList) {
-                if (wp.getName().toLowerCase(Locale.getDefault()).contains(charText)) {
-                    goodsSpecific.add(wp);
-                }
-            }
-        }
-        notifyDataSetChanged();
     }
 
 }
