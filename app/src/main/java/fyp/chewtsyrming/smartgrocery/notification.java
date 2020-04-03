9826510da034cb1786f0ea34beb535d6bf3ca543
alexchew95 <37780051+alexchew95@users.noreply.github.com>
2020-04-03 14:58:27 +0800

    new ui for login and register //todo intergrate register java into login java

---
+++ app/src/main/java/fyp/chewtsyrming/smartgrocery/notification.java
@@ -0,0 +1,17 @@
+package fyp.chewtsyrming.smartgrocery;
+
+import android.app.Application;
+
+import com.onesignal.OneSignal;
+public class notification extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+}
